0cd7055742d46dbedf344fe68bc9a4018ec54792
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-08 21:09:32 +0530

    Added:Welcome Message

---
+++ main/java/AssignmentSeven/UserMaximum.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package AssignmentSeven;
+
+/**
+ * @author saideeksha
+ *
+ */
+public class UserMaximum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Welcome to Test maximum Code");
+	}
+}
